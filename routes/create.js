c3da0d47b9c305b0bf6bfb82f480805513ee7c61
developer-samara-web <developer.samara.web@gmail.com>
2025-02-21 18:29:23 +0400

    update create route

--- routes/create.js
+++ routes/create.js
@@ -1,29 +1,32 @@
-const createInvite = require('@utils/createInvite')
-const connectToDatabase = require('@services/mongoose')
-const User = require('@models/User')
+// Импорты
+const { logs } = require('@utils/logs');
+const { setUser, getUser } = require('@controllers/userController');
+const { getInvite, updateInvite} = require('@controllers/inviteController');
 
-const create = async (req, res, telegram) => {
+// Роут создания приглашения
+const create = async (req, res) => {
 	try {
 		// Получаем id пользователя
-		const { clientId } = req.query
-		// Создаём инвайт
-		const { invite_link } = await createInvite(clientId, telegram, res)
-		// Подключаемся к базе
-        await connectToDatabase()
-		// Создаём новый лид
-		const user = new User({
-            yaid: clientId,
-            invite: invite_link,
-			date: new Date()
-        })
-		// Сохраняем в базе
-		await user.save()
-		// Отдаём инвайт в ответе
-		res.status(200).send(invite_link)
+		const { clientId } = req.query;
+		// Проверка на существование
+		const findUser = await getUser(clientId);
+
+		if (!findUser) {
+			// Получаем свободный инвайт
+			const { _id, href } = await getInvite({ user_id: null });
+			// Создаём нового юзера
+			const user = await setUser(clientId, _id);
+			// Резервируем инватй за юзером
+			await updateInvite(_id, user._id);
+			// Отдаём инвайт в ответе
+			res.status(200).send(href);
+		} else {
+			res.status(200).send(findUser.invite.href);
+		}
 	} catch (e) {
-		res.status(400).send(`Ошибка: не удалось отправить приглашение.`)
-		console.log(e)
+		res.status(400).send(`Не удалось отправить приглашение.`);
+		logs('🟥 <b>ERROR:</b> Не удалось отправить приглашение', e);
 	}
 }
 
-module.exports = create
+module.exports = { create }
