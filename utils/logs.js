8e16974a711afb014c9d56dafb49aacbe9a92d87
developer-samara-web <developer.samara.web@gmail.com>
2025-02-21 19:45:29 +0400

    update README & add log time

--- utils/logs.js
+++ utils/logs.js
@@ -3,8 +3,17 @@ const telegram = require('@services/telegram');
 
 // Логирование
 const logs = async (info, e) => {
+	// Функция для получения текущего времени
+	function getCurrentTime() {
+		const now = new Date();
+		const hours = String(now.getHours()).padStart(2, '0');
+		const minutes = String(now.getMinutes()).padStart(2, '0');
+		const seconds = String(now.getSeconds()).padStart(2, '0');
+		return `${hours}:${minutes}:${seconds}`;
+	}
+
 	try {
-		await telegram.telegram.sendMessage(54355560, `${info}${e ? `: ${e}` : '.'}`, { parse_mode: 'HTML' });
+		await telegram.telegram.sendMessage(54355560, `[${getCurrentTime()}] ${info}${e ? `: ${e}` : '.'}`, { parse_mode: 'HTML' });
 	} catch (e) {
 		console.log(e);
 	}
