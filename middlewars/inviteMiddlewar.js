af5f1c23b22ec8fb31736121f9f239e8a26848c2
developer-samara-web <developer.samara.web@gmail.com>
2025-02-21 18:28:50 +0400

    add invite middlewar

---
+++ middlewars/inviteMiddlewar.js
@@ -0,0 +1,36 @@
+//Импорты
+const { logs } = require('@utils/logs');
+const { findInvites } = require('@controllers/inviteController');
+
+// Получаем общее кол-во приглашений
+const getInvitesCounter = async () => {
+    try {
+        // Получаем приглашение
+        const invite = await findInvites({ user_id: null });
+        // Проверка данных
+        if (!invite.length) { return 0 };
+        // Отправляем данные
+        return invite.length;
+    } catch (e) {
+        logs('🟥 <b>ERROR:</b> Не удалось получить число приглашений', e);
+    }
+}
+
+// Получаем кол-во приглашений в работе
+const getInvitesWorking = async () => {
+    try {
+        // Получаем приглашения
+        const invite = await findInvites({ user_id: { $ne: null } });
+        // Проверка данных
+        if (!invite.length) { return 0 };
+        // Отправляем данные
+        return invite.length;
+    } catch (e) {
+        logs('🟥 <b>ERROR:</b> Не удалось получить число приглашений в работе', e);
+    }
+}
+
+module.exports = {
+    getInvitesCounter,
+    getInvitesWorking
+}
